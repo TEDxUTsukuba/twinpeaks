d4e6264cdb3f3d163d7e9d0bd2c62493d4252c6a
Haruki Kinoshita <harukikinoshita@outlook.com>
2020-06-21 13:27:42 +0900

    Fix talk pages

--- pages/events/2019.vue
+++ pages/events/2019.vue
@@ -1,61 +1,124 @@
 <template>
-  <section>
+  <section class="font-awesome">
     <h1 class="title textoverflow">TEDxUTsukuba 2020<br>CoPhilAction</h1>
     <img id="top" src = "~/assets/banner/2019.jpg" />
-    <section class="hero">
-      <div class="container">
-        <h1 class="title is-1 has-text-centered">
-          Talks
-        </h1>
+    <section class="hero is-large is-black">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 has-text-centered">
+            Talks
+          </h1>
+          <TalkDetails year="2019" />
+        </div>
       </div>
+      <!--
       <div　v-for = 'sp in speakers' :key = 'sp.id'>
         <h1>{{ sp.title }}</h1>
       </div>
+       -->
     </section>
 
-    <section class="hero">
-      <div class="container">
-        <h1 class="title is-1 has-text-centered">
-          Workshop
-        </h1>
+    <section class="hero is-large is-white">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 has-text-centered">
+            Workshop
+          </h1>
+        </div>
       </div>
     </section>
 
-    <section class="hero">
-      <div class="container">
-        <h1 class="title is-1 has-text-centered">
-          Performance
-        </h1>
+    <section class="hero is-large">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 has-text-centered">
+            Performance
+          </h1>
+        </div>
       </div>
     </section>
 
-    <section class="section" >
-      <div class="container">
-        <h1 class="title is-1 has-text-centered">
-          Concept
-        </h1>
-        <h1 class="title is-2 has-text-centered">
-          CoPhilAction
-        </h1>
-        <article>
-          Se et enim senserit. Ubi anim occaecat quibusdam. Quo export praetermissum, o hic concursionibus ne nescius a arbitror si proident tempor legam quo esse iis quid an do quorum iudicem do offendit tamen quis iudicem veniam est non eram cillum a constias, nam velit voluptatibus. Occaecat ita quis, mentitum sunt proident vidisse.Ita vidisse ut admodum ab doctrina est anim iudicem te fore e admodum, eu anim mentitum admodum ut probant irure consequat commodo ut noster arbitror sed quid dolor, multos officia in despicationes, e probant ad occaecat. Nisi mentitum litteris ita illum nescius fidelissimae se sint ullamco et export malis an ubi magna sint labore incurreret, deserunt o deserunt, e magna si eram iis an irure firmissimum, nulla e officia. Ipsum sed quibusdam eu sint nam hic fugiat imitarentur, ullamco magna nam deserunt efflorescere, consequat a nisi. Mentitum sint nostrud o summis voluptate comprehenderit, proident sed cupidatat, ne aute appellat, nostrud se quorum, tamen ex ex quid doctrina, aliqua incurreret singulis ut o malis nulla ea pariatur.
-        </article>
+    <section class="hero is-large" >
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 has-text-centered">
+            Concept
+          </h1>
+          <h1 class="title is-2 has-text-centered">
+            CoPhilAction
+          </h1>
+          <article>
+            Se et enim senserit. Ubi anim occaecat quibusdam. Quo export praetermissum, o hic concursionibus ne nescius a arbitror si proident tempor legam quo esse iis quid an do quorum iudicem do offendit tamen quis iudicem veniam est non eram cillum a constias, nam velit voluptatibus. Occaecat ita quis, mentitum sunt proident vidisse.Ita vidisse ut admodum ab doctrina est anim iudicem te fore e admodum, eu anim mentitum admodum ut probant irure consequat commodo ut noster arbitror sed quid dolor, multos officia in despicationes, e probant ad occaecat. Nisi mentitum litteris ita illum nescius fidelissimae se sint ullamco et export malis an ubi magna sint labore incurreret, deserunt o deserunt, e magna si eram iis an irure firmissimum, nulla e officia. Ipsum sed quibusdam eu sint nam hic fugiat imitarentur, ullamco magna nam deserunt efflorescere, consequat a nisi. Mentitum sint nostrud o summis voluptate comprehenderit, proident sed cupidatat, ne aute appellat, nostrud se quorum, tamen ex ex quid doctrina, aliqua incurreret singulis ut o malis nulla ea pariatur.
+          </article>
+        </div>
       </div>
     </section>
 
-    <section class="hero">
-      <div class="container">
-        <h1 class="title is-1 has-text-centered">
-          Attendee's voice
-        </h1>
+    <section class="hero is-large">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 has-text-centered">
+            Attendee's voice
+          </h1>
+          <div class="tile is-ancestor">
+            <div class="tile is-vertical is-8">
+              <div class="tile">
+                <div class="tile is-parent">
+                  <article class="tile is-child notification">
+                    <p class="title is-2">
+                      <font-awesome-layers class="fa-lg fa-pull-left" style="margin-right: 20px;">
+                        <font-awesome-icon :icon="['fas', 'quote-left']" style="color: red;" />
+                      </font-awesome-layers>
+                      A day full of discoveries for all of us to share.
+                    </p>
+                    <p class="subtitle">20代 学生</p>
+                  </article>
+
+                </div>
+                <div class="tile is-parent">
+                  <article class="tile is-child notification is-black">
+                    <p class="title is-2">
+                      <font-awesome-layers class="fa-lg fa-pull-left" style="margin-right: 20px;">
+                        <font-awesome-icon :icon="['fas', 'quote-left']" style="color: red;" />
+                      </font-awesome-layers>
+                      A day full of discoveries for all of us to share.
+                    </p>
+                  </article>
+                </div>
+              </div>
+              <div class="tile is-parent">
+                <article class="tile is-child notification">
+                  <p class="title is-2">
+                    <font-awesome-layers class="fa-lg fa-pull-left" style="margin-right: 20px;">
+                      <font-awesome-icon :icon="['fas', 'quote-left']" style="color: red;" />
+                    </font-awesome-layers>
+                    A day full of discoveries for all of us to share.
+                  </p>
+                </article>
+              </div>
+            </div>
+            <div class="tile is-parent">
+              <article class="tile is-child notification" style="background-color: red;">
+                <p class="title is-2 has-text-white">
+                  <font-awesome-layers class="fa-lg fa-pull-left" style="margin-right: 20px;">
+                    <font-awesome-icon :icon="['fas', 'quote-left']" />
+                  </font-awesome-layers>
+                  A day full of discoveries for all of us to share.
+                </p>
+              </article>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 
-    <section class="hero">
-      <div class="container">
-        <h1 class="title is-1 has-text-centered">
-          About info
-        </h1>
+    <section class="hero is-large">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1 has-text-centered">
+            About
+          </h1>
+        </div>
       </div>
     </section>
 
@@ -65,10 +128,11 @@
 
 <script>
     import Events from '~/components/Events.vue'
+    import TalkDetails from '~/components/TalkDetails.vue'
 
     export default {
       components: {
-        Events
+        Events, TalkDetails
       },
       data(){
         return {
@@ -101,5 +165,9 @@ img#top {
   left: 10vw;
   position: absolute;
   top: 25vh;
+}
+.is-red {
+  background-color: red;
+  color: white;
 }
 </style>
--- pages/talks.vue
+++ pages/talks.vue
@@ -1,49 +1,44 @@
 <template>
-  <section class="section">
-    <div class="columns is-variable is-6 is-multiline is-centered">
-      <div class="column is-one-third" v-for = 'sp in speakers'
-      :key = 'sp.id'>
-        <div class="card-image">
-          <figure class="image is-3by2">
-            <img :src = 'sp.url' />
-          </figure>
-        </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">{{ sp.title }}</p>
-              <p class="subtitle is-6">{{ sp.name }}</p>
-            </div>
-          </div>
-          <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Phasellus nec iaculis mauris.
-          </div>
-        </div>
-      </div>
+  <section>
+    <div v-for="(talk, index) in talkList" :key="index">
+      <h1 class="title">{{ talk.title }}</h1>
+      <h1 class="subtitle">{{ talk.speaker_name }}</h1>
+      <p>{{ talk.description_ja }}</p>
+      <span class="tag">{{ talk.year }}</span>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  data(){
-    return {
-      speakers: [
-        { id:1, title: "私は靴を履かないバーテンダー", name: "松島壮志", url: '/_nuxt/assets/speakers/2019/sp_2019_1.jpg' },
-        { id:2, title: "私は靴を履かないバーテンダー", name: "山田玲司", url: '/_nuxt/assets/speakers/2019/sp_2019_2.jpg' },
-        { id:3, title: "私は靴を履かないバーテンダー", name: "Leonid Dimaano / Annet Nakkazi", url: '/_nuxt/assets/speakers/2019/sp_2019_3-1.jpg' },
-        { id:4, title: "私は靴を履かないバーテンダー", name: "Carol Inugai Dixon", url: '/_nuxt/assets/speakers/2019/sp_2019_4.jpg' }
-      ],
-      topImg: require(`~/assets/speakers/2019/sp_2019_1.jpg`)
+  props: {
+    year: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    talkList() {
+      return this.$store.getters[
+        'talk/showAllTalks'
+      ](this.year)
     }
   }
 }
 </script>
 
-<style scoped>
-.card {
-  /* margin: 1.25vw; */
-  padding: 0;
+<!--
+<template>
+  <section>
+    <TalkDetail year="2019" />
+  </section>
+</template>
+
+<script>
+import TalkDetail from '~/components/TalkDetail.vue'
+export default {
+  components: {
+    TalkDetail
+  }
 }
-</style>
+</script> -->
