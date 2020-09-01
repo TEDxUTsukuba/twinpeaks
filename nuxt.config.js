ec45426ad03b491063ba0d8e3a76211a3576b225
Haruki Kinoshita <harukikinoshita@outlook.com>
2020-09-02 03:52:48 +0900

    Add modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   ** Global CSS
   */
   css: [
-    { src: 'bulma/bulma.sass', lang: 'sass' }
+    { src: '~/assets/style.scss', lang: 'scss' }
   ],
   /*
   ** Plugins to load before mounting the App
