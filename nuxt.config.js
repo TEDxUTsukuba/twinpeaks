b049012ac47e1ab651a5a6d79d60439ea96b47de
Haruki Kinoshita <harukikinoshita@outlook.com>
2020-11-08 03:48:14 +0900

    Add OGP

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,30 +10,47 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'twitter:site', name: 'twitter:site', content: 'tedxutsukuba' },
+      // { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { 
+        hid: 'twitter:card', 
+        name: 'twitter:card', 
+        content: 'summary_large_image' 
+      },
+      { 
+        hid: 'twitter:site', 
+        name: 'twitter:site', 
+        content: 'tedxutsukuba' 
+      },
       {
         hid: 'og:title',
-          property: 'og:title',
-            content: 'TEDxUTsukuba'
+        property: 'og:title',
+        content: 'TEDxUTsukuba'
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
       },
       {
         hid: 'og:url',
-          property: 'og:url',
-            content: 'https://tedxutsukuba.com'
+        property: 'og:url',
+        content: 'https://tedxutsukuba.com'
       },
       {
         hid: 'og:description',
-          property: 'og:description',
-            content: '価値あるアイディアを、つくばから。Ideas worth spreading. '
+        property: 'og:description',
+        content: '価値あるアイディアを、つくばから。Ideas worth spreading. '
       },
       {
         hid: 'og:image',
-          property: 'og:image',
-         content: 'https://teraoka.netlify.app/_nuxt/img/bba6327.png'
+        property: 'og:image',
+        content: 'https://teraoka.netlify.app/_nuxt/img/bba6327.png'
       },
-      { hid: 'og:site_name', name: 'og:site_name', content: 'TEDxUTsukuba Official Website' }
+      { 
+        hid: 'og:site_name', 
+        name: 'og:site_name', 
+        content: 'TEDxUTsukuba Official Website' 
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
