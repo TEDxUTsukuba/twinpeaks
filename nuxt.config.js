43341c03d79848ad022f8e30a31204279c722571
Haruki Kinoshita <harukikinoshita@outlook.com>
2020-11-07 22:51:37 +0900

    Update meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,29 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'twitter:card', name: 'twitter:card', content: summary_large_image },
+      { hid: 'twitter:site', name: 'twitter:site', content: tedxutsukuba },
+      {
+        hid: 'og:title',
+          property: 'og:title',
+            content: 'TEDxUTsukuba'
+      },
+      {
+        hid: 'og:url',
+          property: 'og:url',
+            content: 'https://tedxutsukuba.com'
+      },
+      {
+        hid: 'og:description',
+          property: 'og:description',
+            content: '価値あるアイディアを、つくばから。Ideas worth spreading. '
+      },
+      {
+        hid: 'og:image',
+          property: 'og:image',
+         content: 'https://teraoka.netlify.app/_nuxt/img/bba6327.png'
+      },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'TEDxUTsukuba Official Website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
