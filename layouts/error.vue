a83e6780314e8fce7f83731d43f3eefb412e7306
Haruki Kinoshita <harukikinoshita@outlook.com>
2021-01-21 07:40:52 +0900

    Add error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,24 +1,39 @@
 <template>
-  <section class="section has-text-centered" style="padding: 30vh 0;">
-    <h1 class="title is-1">Oops!</h1>
-    <h1 class="subtitle">the page you're looking for doesn't exist or unavailable. </h1>
+  <section class="section has-text-centered" id="all">
+    <img src="404.png" />
+    <p class="has-text-weight-light is-size-4">The page you're looking for doesn't exist or unavailable. </p>
+    <hr>
+    <p>Need a little break? Watch <a href="https://youtu.be/ToJD5r2SmwI" target="_blank" rel="noopener noreferrer">this</a>.</p>
+
   </section>
 </template>
 
 <script>
-import Logo from '@/assets/logo/u_white.svg'
 export default {
-  components: {
-    Logo
-  },
   props: [
-    {
-      error: {
-        type: Object,
-        default: null
-      }
-    }
+    'error'
   ],
-  layout: ''
+  head () {
+    return {
+      title: 'Page Not Found',
+      meta: [
+        { hid: 'description', name: 'description', content: 'The page you\'re looking for doesn\'t exist or unavailable.' }
+      ]
+    }
+  }
 }
 </script>
+
+<style scoped>
+@keyframes fadeIn {
+    0% {
+        opacity:0;
+    }
+    100% {
+        opacity:1;
+    }
+}
+#all {
+  animation: 2s fadeIn;
+}
+</style>
