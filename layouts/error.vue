3eb781c2b5bfef6ad66fe6ddfaaec7fd968effc9
Haruki Kinoshita <harukikinoshita@outlook.com>
2021-01-21 07:01:13 +0900

    Minor change

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="section has-text-centered" style="padding: 20vh 0;">
+  <section class="section has-text-centered" style="padding: 30vh 0;">
     <h1 class="title is-1">Oops!</h1>
     <h1 class="subtitle">the page you're looking for doesn't exist or unavailable. </h1>
   </section>
