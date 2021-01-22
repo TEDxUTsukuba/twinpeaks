2b9b2125684256059cc38b2569451e209cd5c1c6
Haruki Kinoshita <harukikinoshita@outlook.com>
2021-01-23 02:44:19 +0900

    Add contents

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="section has-text-centered" id="all">
-    <img src="404.png" />
-    <p class="has-text-weight-light is-size-4">The page you're looking for doesn't exist or unavailable. </p>
+    <figure>
+      <img src="404.png" alt="Page Not Found" />
+    </figure>
+    <p class="has-text-weight-light is-size-4" v-if="$i18n.locale == 'en'">The page you're looking for doesn't exist or unavailable. </p>
+    <p class="has-text-weight-light is-size-4" v-if="$i18n.locale == 'ja'">お探しのページは見つかりません。</p>
     <hr>
     <p>Need a little break? Watch <a href="https://youtu.be/8S0FDjFBj8o" target="_blank" rel="noopener noreferrer">this</a>.</p>
 
