f2937aaf57450c11584a1a4b8a12a37dca4f654a
HarukiKinoshita <harukikinoshita@outlook.com>
2021-03-19 19:27:59 +0900

    Connect to Google Sheets

--- pages/events/_event.vue
+++ pages/events/_event.vue
@@ -1,85 +1,95 @@
 <template>
   <div :id="id">
-    <div v-for="(event, index) in eventData" :key="index">
-      <div class="columns">
-        <div class="column wallpaper-wrapper">
-          <img class="wallpaper" src="https://images.pexels.com/photos/3184302/pexels-photo-3184302.jpeg?cs=srgb&dl=pexels-fauxels-3184302.jpg&fm=jpg">
-        </div>
-        <aside class="column container is-5" style="display: flex; justify-content: middle; align-items: center;">
-          <p class="is-uppercase">3 days to go</p><br>
-          <h1 class="title is-3">{{ event.name }}</h1>
-          {{ $route.params.id }}
-        </aside>
+    <div class="columns">
+      <div class="column wallpaper-wrapper">
+        <img class="wallpaper" src="https://images.pexels.com/photos/3184302/pexels-photo-3184302.jpeg?cs=srgb&dl=pexels-fauxels-3184302.jpg&fm=jpg">
       </div>
-      <div class="columns">
-        <div class="column is-7">
-          <h1 class="is-size-4"><i class="mdi mdi-export-variant" /></h1>
+      <aside class="column container is-5" style="display: flex; justify-content: middle; align-items: center;">
+        <div>
+          <p class="is-uppercase">N days to go</p><br>
+          <h1 class="title is-3">{{ eventData.name }}</h1>
         </div>
-        <div class="column is-5 ">
-          <a :href="event.signup.url" class="level-item button is-white is-gradient is-rounded is-fullwidth">Sign up</a>
-        </div>
+      </aside>
+    </div>
+    <div class="columns">
+      <div class="column is-7">
+        <h1 class="is-size-4"><i class="mdi mdi-export-variant" /></h1>
       </div>
-      <hr style="margin: 0.5rem 0 1rem 0;">
-      <div class="columns is-variable is-5">
-        <div class="container column is-7">
-            <p v-if="$i18n.locale == 'ja'" v-html='event.description.long_ja'></p>
-            <p v-if="$i18n.locale == 'en'" v-html='event.description.long_en'></p>
-            <hr>
-            Share With Friends
-            <br>
-            <a href="" class="is-size-4 has-text-dark"><i class="mdi mdi-twitter" /></a>
-            <a href="" class="is-size-4 has-text-dark"><i class="mdi mdi-facebook" /></a>
-            <a href="" class="is-size-4 has-text-dark"><i class="mdi mdi-link-variant" /></a>
-        </div>
-        <aside class="container column is-5">
-          <p class="is-size-5">{{ event.date.day.replace(regex, '/') }}</p>
-          <p class=""><span v-if="event.date.from">{{ event.date.from }}</span><span v-if="event.date.to"> - {{ event.date.to }}</span></p>
-          <a href="" class="is-size-6">Add to Calendar</a>
+      <div class="column is-5 has-text-centered" v-if="eventData.isSignupRequired = 'true'">
+        <a :href="eventData.signup_url" target="_blank" class="level-item button is-white is-gradient is-rounded is-fullwidth">Sign up</a>
+        <!-- <span class="is-size-7 has-text-grey-light">Sign up before {{ new Date(eventData.signup_deadline).toLocaleDateString().replace(regex, '/') }}</span> -->
+      </div>
+    </div>
+    <hr style="margin: 0.5rem 0 1rem 0;">
+    <div class="columns is-variable is-5">
+      <div class="container column is-7">
+          <p v-if="$i18n.locale == 'ja'" v-html='eventData.description_ja'></p>
+          <p v-if="$i18n.locale == 'en'" v-html='eventData.description_en'></p>
           <hr>
-          <p v-if="event.type === 'virtual'" class="is-size-5">
-            Virtual Event<br>
-            <span class="is-size-6">Webcast</span><br>
-            <a :href="event.webcast.webcast_url" class="is-size-6">{{ event.webcast.name }}</a>
-          </p>
-          <p v-else-if="event.type === 'hybrid'" class="is-size-5">
-            Hybrid Event<br>
-            <span class="is-size-6">Webcast</span><br>
-            <a :href="event.webcast.webcast_url" class="is-size-6">Open in {{ event.webcast.name }}</a><br>
-            <span class="is-size-6">{{ event.location.name }}</span><br>
-            <a :href="event.location.googlemaps" class="is-size-6">Open in Google Maps</a>
-          </p>
-          <p v-else>
-            <span class="is-size-6">{{ event.location.name }}</span><br>
-            <a :href="event.location.googlemaps" class="is-size-6">Open in Google Maps</a>
-          </p>
-        </aside>
+          Share With Friends
+          <br>
+          <a href="" target="_blank" class="is-size-4 has-text-dark"><i class="mdi mdi-twitter" /></a>
+          <!-- <a href="https://twitter.com/intent/tweet?button_hashtag=TEDxUTsukuba&ref_src=twsrc%5Etfw" class="button is-rounded" data-show-count="false"><i class="mdi mdi-twitter" />Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
+          <a href="" target="_blank" class="is-size-4 has-text-dark"><i class="mdi mdi-facebook" /></a>
+          <a href="" target="_blank" class="is-size-4 has-text-dark"><i class="mdi mdi-link-variant" /></a>
       </div>
+      <aside class="container column is-5">
+        <p class="is-size-5">{{ new Date(eventData.date).toLocaleDateString().replace(regex, '/') }}</p>
+        <p class=""><span v-if="eventData.from">{{ eventData.from }}</span><span v-if="eventData.to"> - {{ eventData.to }}</span></p>
+        <a href="" class="is-size-6">Add to Calendar</a>
+        <hr>
+        <p v-if="eventData.type === 'virtual'" class="is-size-5">
+          Virtual Event<br>
+          <span class="is-size-6">Webcast</span><br>
+          <a :href="eventData.webcast_url" target="_blank" class="is-size-6">{{ eventData.webcast }}</a>
+        </p>
+        <p v-else-if="eventData.type === 'hybrid'" class="is-size-5">
+          Hybrid Event<br>
+          <span class="is-size-6">Webcast</span><br>
+          <a :href="eventData.webcast_url" target="_blank" class="is-size-6">Open in {{ eventData.webcast }}</a><br>
+          <span class="is-size-6">{{ eventData.location }}</span><br>
+          <a :href="eventData.googlemaps" target="_blank" class="is-size-6">Open in Google Maps</a>
+        </p>
+        <p v-else>
+          <span class="is-size-6">{{ eventData.location }}</span><br>
+          <a :href="eventData.googlemaps" target="_blank" class="is-size-6">Open in Google Maps</a>
+        </p>
+        <hr>
+        <p v-if="eventData.isFree == 'true'" class="is-size-5 mb-6">
+          Free
+        </p>
+        <p v-else class="is-size-6 mb-6">
+          Adult: JPY {{ eventData.fee_adult}}<br>
+          Student: JPY {{ eventData.fee_student }}
+        </p>
+        <br><br>
+      </aside>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   components: {
   },
   data () {
     return {
+      eventData: [],
       isModalActive: false,
       id: this.$route.params.event,
       regex: /-/g,
     }
   },
   computed: {
-    eventData() {
-      return this.$store.getters['upcomingeventfromjson/findById'](Number(this.$route.params.event))
-    }
   },
-  methods: {
-    getCurrentUrl() {
-      if (process.browser) {
-        return window.location.href
-      }
+  async asyncData({ $axios }) {
+    const api_url = 'https://script.googleusercontent.com/macros/echo?user_content_key=m8yBv_e7D8GB8kxpVhJT-U_oAyGXB7vVvstpnRznGYjDntppdw33CjFjg4DrUo5vufdNWkqQBfVaruuzitG4vZMH8UCTNYFYm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHtABZsLdz3HrzWV3XoW6E7FN53ubmn6Q9vLnesjgoy7SWFL9jZG44if4mn39WErlUHcG6vFq3AroXcexxZxpVH-TREc3TXom9z9Jw9Md8uu&lib=MUPcWAKUzMkGApY5JUER5gEPjB6H_eQBZ';
+    const res = await $axios.$get(api_url)
+    .catch(function(error) {
+      console.log('Error occurred while getting data')
+    });
+    return {
+      eventData: res[1]
     }
   }
 }
