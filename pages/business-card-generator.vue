ef1627dad9ffff1e7aa275777a0846b9a84235f7
HarukiKinoshita <harukikinoshita@outlook.com>
2021-05-16 03:13:16 +0900

    Add functions

--- pages/business-card-generator.vue
+++ pages/business-card-generator.vue
@@ -7,101 +7,142 @@
       <h1 class="subtitle is-size-4 has-text-weight-light">
         Create your own <span class="has-text-weight-bold">digital</span> business card. 
       </h1>
-      <div class="columns is-vcentered">
-        <div class="column is-7-desktop is-6-tablet notification is-light">
-          <div style="max-width: 400px; margin: 1rem auto 1rem auto;">
-            <div v-if="isValid == 'valid'">
+      <div class="columns is-multiline is-variable is-6">
+        <div class="column is-5-desktop is-12-tablet">
+          <div class="nmp-card-light" style="max-width: 480px; margin: 1rem auto 1rem auto;">
+            <b-field
+              label="Name"
+              position="is-centered"
+              :type="fieldType"
+              :message="fieldMessage"
+            >
+              <b-input expanded id="input"
+                size="is-medium"
+                placeholder="John Doe"
+                type="text"
+                v-model="inputName"
+              />
+            </b-field>
+
+            <b-field label="Font Size">
+              <b-slider v-model="fontsize" :min="76" :max="140" :step="8" ticks rounded>
+              </b-slider>
+            </b-field>
+          </div>
+          <div class="nmp-card-light" style="max-width: 480px; margin: 1rem auto 1rem auto;">
+            <b-field
+              label="Position"
+              position="is-centered"
+            >
+              <b-input expanded id="input"
+                size="is-medium"
+                placeholder="Curator"
+                type="text"
+                v-model="inputPosition"
+              />
+            </b-field>
+          </div>
+          <div class="nmp-card-light" style="max-width: 480px; margin: 1rem auto 2rem auto;">
+            <b-field
+              label="Email"
+              position="is-centered"
+            >
+              <b-checkbox v-model="insertEmail">tedxutsukuba&#64;gmail.com</b-checkbox>
+            </b-field>
+            <!-- <div v-if="isValid == 'valid'">
               <b-tag
                 class="is-success is-medium"
                 size="is-medium"
               >
                 <i class="mdi mdi-checkbox-marked-circle" /> Valid
               </b-tag>
-            </div>
-            <div v-if="isValid == 'invalid'">
-              <b-tag
-                class="is-danger is-medium"
-                size="is-medium"
-              >
-                <i class="mdi mdi-alert-circle" /> Numbers and emojis are not allowed. 
-              </b-tag>
-            </div>
-            <div v-if="isValid == 'blank'">
-              <b-tag
-                class="is-white is-medium"
-                size="is-medium"
-              >
-                <i class="mdi mdi-alert-circle" /> Type your name.
-              </b-tag>
-            </div>
-            <br>
-            <b-field grouped position="is-centered">
-              <b-input expanded id="input"
-                size="is-medium"
-                placeholder="John Doe"
-                type="text"
-                v-model="inputText"
-              />
-            </b-field>
-            <br>
-            <b-field label="Font Size">
-              <b-slider v-model="fontsize" :min="76" :max="140" :step="8" ticks>
-              </b-slider>
-            </b-field>
+            </div> -->
           </div>
         </div>
-        <div class="column is-5-desktop is-6-tablet">
-          <div id="konvaCanvas" style="margin: auto;">
-            <v-stage ref="stage" :config="{
-              scale: {x: 0.2, y: 0.2},
-              width: this.width,
-              height: this.height,
-              id: `${this.inputText}`
-            }">
-              <v-layer ref="layer">
-                <v-image :config="{
-                  image: bgImage
-                }"/>
-                <!-- <v-circle :config="configCircle"></v-circle> -->
-                <v-text v-if="isValid == 'valid'"
-                :config="{
-                  text: `${this.inputText}`, 
-                  align: 'center',
-                  verticalAlign: 'middle',
-                  fontFamily: `${selectFont}`,
-                  fontSize: this.fontsize,
-                  fontStyle: 'bold',
-                  fill: '#fff',
-                  width: this.width,
-                  height: this.height,
-                  offsetY: 14,
-                  id: `${this.inputText}`
-                }"/>
-              </v-layer>
-            </v-stage>
-          </div>
-          <div style="margin: 2rem auto 2rem auto;">
-            <b-button id="save" @click="save" class="button is-primary has-text-weight-bold" size="is-medium" icon-left="download">
-              Save Image
-            </b-button>
-          </div>
+        <div class="column is-7-desktop is-12-tablet">
+          <b-tooltip
+            label="Tap 'Save' button to save this image."
+            multilined>
+            <div id="konvaCanvas" style="margin: auto;">
+              <v-stage ref="stage" :config="{
+                scale: {x: 0.4, y: 0.4},
+                width: this.width,
+                height: this.height,
+                id: `${this.inputName}`
+              }">
+                <v-layer ref="layer">
+                  <v-image :config="{
+                    image: bgImage
+                  }"/>
+                  <!-- <v-circle :config="configCircle"></v-circle> -->
+                  <v-text v-if="isValid == true"
+                  :config="{
+                    text: `${this.inputName}`, 
+                    align: `${this.align}`,
+                    padding: this.padding,
+                    verticalAlign: 'middle',
+                    fontFamily: `${selectFont}`,
+                    fontSize: this.fontsize,
+                    fontStyle: 'bold',
+                    fill: '#fff',
+                    width: this.width,
+                    height: this.height,
+                    offsetY: 54,
+                  }"/>
+                  <v-text
+                    :config="{
+                      text: `${this.inputPosition}`, 
+                      align: `${this.align}`,
+                      padding: this.padding,
+                      verticalAlign: 'middle',
+                      fontFamily: `${selectFont}`,
+                      fontSize: 60,
+                      fontStyle: 'bold',
+                      fill: '#90a4ae',
+                      width: this.width,
+                      height: this.height,
+                      offsetY: -64,
+                    }"/>
+                  <v-text v-if="insertEmail"
+                    :config="{
+                      text: `${this.inputEmail}`, 
+                      align: `${this.align}`,
+                      padding: this.padding,
+                      verticalAlign: 'bottom',
+                      fontFamily: `${selectFont}`,
+                      fontSize: 40,
+                      fontStyle: 'normal',
+                      fill: '#eceff1',
+                      width: this.width,
+                      height: this.height,
+                      offsetY: 90,
+                    }"/>
+                </v-layer>
+              </v-stage>
+            </div>
+           </b-tooltip>
+          <p class="has-text-grey-light" style="margin: 2rem 0 1rem 0;">Your card will look like this.</p>
+          <b-button id="save" @click="save" class="button is-primary has-text-weight-bold" size="is-medium" icon-left="download">
+            Save
+          </b-button>
         </div>
       </div>
       <v-stage style="display: none;" ref="stage_fullsize" :config="{
         scale: {x: 1, y: 1},
         width: this.width,
         height: this.height,
-        id: `${this.inputText}`
+        id: `${this.inputName}`
       }">
         <v-layer ref="layer">
           <v-image :config="{
             image: bgImage
           }"/>
           <!-- <v-circle :config="configCircle"></v-circle> -->
-          <v-text v-if="isValid == 'valid'"
+          <v-text v-if="isValid == true"
           :config="{
-            text: `${this.inputText}`, 
-            align: 'center',
+            text: `${this.inputName}`, 
+            align: `${this.align}`,
+            padding: this.padding,
             verticalAlign: 'middle',
             fontFamily: `${selectFont}`,
             fontSize: this.fontsize,
@@ -109,16 +150,43 @@
             fill: '#fff',
             width: this.width,
             height: this.height,
-            offsetY: 14,
-            id: `${this.inputText}`
+            offsetY: 54,
           }"/>
+          <v-text
+            :config="{
+              text: `${this.inputPosition}`, 
+              align: `${this.align}`,
+              padding: this.padding,
+              verticalAlign: 'middle',
+              fontFamily: `${selectFont}`,
+              fontSize: 60,
+              fontStyle: 'bold',
+              fill: '#90a4ae',
+              width: this.width,
+              height: this.height,
+              offsetY: -64,
+            }"/>
+          <v-text v-if="insertEmail"
+            :config="{
+              text: `${this.inputEmail}`, 
+              align: `${this.align}`,
+              padding: this.padding,
+              verticalAlign: 'bottom',
+              fontFamily: `${selectFont}`,
+              fontSize: 40,
+              fontStyle: 'normal',
+              fill: '#eceff1',
+              width: this.width,
+              height: this.height,
+              offsetY: 90,
+            }"/>
         </v-layer>
       </v-stage>
     </section>
     <footer class="has-text-centered" style="padding: 1rem;">
       <div class="content has-text-centered">
         <p class="has-text-grey-light">
-          TEDxUTsukuba Digital Business Card Generator<br><span class="has-text-weight-bold">v0.1.0</span> Public Beta
+          TEDxUTsukuba Digital Business Card Generator<br><span class="has-text-weight-bold">v0.1.1</span> Public Beta
         </p>
         <p class="has-text-grey" style="margin-bottom: 1rem;"><span class="">Made with &#9829; by <nuxt-link to="./about/harry" class="has-text-weight-bold">Harry</nuxt-link></span></p>
         <!-- <details>
@@ -151,20 +219,27 @@ export default {
       { 
         hid: 'description', 
         name: 'description', 
-        content: 'Type. Save. Share. Create your own business card online. '
+        content: 'Type. Save. Share. Create your own digital business card. '
       },
     ]
   },
   data() {
     return {
-      isValid: "blank",
-      status: "Type your name.",
+      isValid: false,
+      // status: "Blank",
       aspect: 'Business Card',
-      inputText: '',
+      inputName: '',
+      inputPosition: '',
+      inputEmail: 'tedxutsukuba@gmail.com',
+      insertEmail: true,
+      fieldMessage: '',
+      fieldType: '',
       bgImage: '',
       width: 1365,
       height: 825,
-      fontsize: 116,
+      fontsize: 108,
+      padding: 72,
+      align: 'left',
       configCircle: {
         x: 100,
         y: 100,
@@ -177,17 +252,20 @@ export default {
     }
   },
   watch: {
-    inputText: function(val) {
+    inputName: function(val) {
       // console.log(val, val.length)
       if (!val.trim() || val.length == 0) {
-        this.isValid = "blank"
-        this.status = "Type Something. "
-      } else if (val.match(/^[\D]{1,}$/i) == null || val.match(/\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?|\p{Emoji_Presentation}|\p{Emoji}\uFE0F/gu)) {
-        this.isValid = "invalid"
-        this.status = "Numbers and Emojis are not allowed."
+        this.fieldType = 'is-danger'
+        this.fieldMessage = "Type something."
+      } else if  (val.match(/^[\D]{1,}$/i) == null || val.match(/\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?|\p{Emoji_Presentation}|\p{Emoji}\uFE0F/gu)) {
+        this.fieldType = 'is-danger'
+        this.fieldMessage = "Numbers and Emojis are not allowed."
+        // this.isValid = "invalid"
+        // this.status = "Numbers and Emojis are not allowed."
       } else {
-        this.isValid = "valid"
-        this.status = ""
+        this.isValid = true
+        this.fieldType = ''
+        this.fieldMessage = ""
       }
     }
   },
@@ -195,9 +273,9 @@ export default {
     save () {
       const stage = this.$refs.stage_fullsize.getStage();
       console.log(stage);
-      localStorage.setItem(this.inputText.replace(/ /g, "_") + '_from_TEDxUTsukuba', JSON.stringify(stage));
+      localStorage.setItem(this.inputName.replace(/ /g, "_") + '_from_TEDxUTsukuba', JSON.stringify(stage));
       var link = document.createElement('a');
-      link.download = this.inputText.replace(/ /g, "_") + '_from_TEDxUTsukuba';
+      link.download = this.inputName.replace(/ /g, "_") + '_from_TEDxUTsukuba';
       link.href = stage.toDataURL();;
       document.body.appendChild(link);
       link.click();
@@ -213,8 +291,7 @@ export default {
       const data = localStorage.getItem('storage') || '[]';
       this.list = JSON.parse(data);
     }
-    if (this.$ua.isFromIos() && this.$ua.browser() != 'Safari') this.$buefy.notification.open({ message: "Some browsers are not comparable with this generator. Try using Safari!", type: 'is-warning', position: 'is-bottom-right' })
-    if (this.$ua.isFromAndroidOs()) this.$buefy.notification.open({ message: "Some browsers are not comparable with this generator.", type: 'is-warning', position: 'is-bottom-right' })
+    if (!this.$ua.isFromPc()) this.$buefy.notification.open({ message: "This generator does not fully comparative with mobile browsers. Try using on your PCs. ", type: 'is-warning', position: 'is-bottom-right', autoClose: true, duration: 5000})
   },
   computed: {
     selectFont() {
@@ -254,8 +331,10 @@ export default {
     font-weight: bold;
   }
   #konvaCanvas {
-    width: 273px;
-    height: 165px;
+    /* width: 273px; */
+    /* height: 165px; */
+    width: 546px;
+    height: 330px;
     margin: 4rem auto 2rem auto;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   }
