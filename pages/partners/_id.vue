1c2cea18f545891e4129a9770a3445cdaf0be956
HarukiKinoshita <harukikinoshita@outlook.com>
2021-07-18 00:31:02 +0900

    Add social media link

--- pages/partners/_id.vue
+++ pages/partners/_id.vue
@@ -2,24 +2,25 @@
   <section id="wrapper-dark">
     <section class="section has-text-centered">
       <p class="has-text-primary mb-2">TEDxUTsukuba Partner</p>
-      <h1 class="title is-2">{{ partnerDetail.name }}</h1>
-    </section>
-    <section class="section has-text-centered-mobile has-text-right">
-      <p class="has-text-left">
+      <h1 class="title is-2 mb-6">{{ partnerDetail.name }}</h1>
+      <p class="has-text-left mb-6">
         大同特殊鋼は素材の力で様々な産業を支える世界最大級の特殊鋼専業メーカーです。
         熱に強い、サビにくいなど様々な機能を持つ特殊鋼は、ボールペンの先から、スマホ、パソコン、自動車、航空機など皆さんの身近なところで活躍しています。
         TEDの目的や精神には当社の経営理念に通じるものがあり、主催・運営を担う学生やスピーカーの皆さんの熱意に賛同して２０１５年から協賛を続けてきました。
         大同特殊鋼はTEDxNagoyaUの熱意を応援します。
       </p>
+      <p class="is-size-7 mb-2" style="letter-spacing: 2px;">Discover more about {{ partnerDetail.name }}</p>
+      <b-button type="is-light is-info" icon-left="mdi mdi-twitter" class="my-1"><span v-if="!$ua.isFromSmartphone()">Twitter</span></b-button>
+      <b-button type="is-light is-warning" icon-left="mdi mdi-instagram" class="my-1"><span v-if="!$ua.isFromSmartphone()">Instagram</span></b-button>
+      <b-button type="is-light is-danger" icon-left="mdi mdi-youtube" class="my-1"><span v-if="!$ua.isFromSmartphone()">YouTube</span></b-button>
       <b-button 
         @click="countGA(partnerDetail.url, partnerDetail.name)"
-        :label="$t('button.visitwebsite')"
-        icon-right="open-in-new"
+        icon-left="earth"
         iconPack="mdi"
-        type="is-white is-small is-rounded"
-        outlined
-        class="mt-5"
+        type="is-light"
+        class="my-1"
       >
+        <span v-if="!$ua.isFromSmartphone()">{{ $t('button.visitwebsite') }}</span>
       </b-button>
       <div style="max-width: 640px; margin: auto;">
         <figure class="image is-3by4 my-6">
