87cab3632b1ad7e69b75d0443411a6b6ef508659
HarukiKinoshita <harukikinoshita@outlook.com>
2021-07-20 14:50:07 +0900

    Fix OGP

--- pages/about/_slug.vue
+++ pages/about/_slug.vue
@@ -130,14 +130,14 @@ export default {
     return {
       title: this.meta.title,
       meta: [
-        { hid: "description", name: "description", content: this.meta.description},
-        { hid: "og:description", property: "og:description", content: this.meta.description},
+        { hid: "description", name: "description", content: this.member.family_name_ja + ' ' + this.member.given_name_ja + ' | ' + this.member.role + ' | ' + this.meta.description },
+        { hid: "og:description", property: "og:description", content: this.member.family_name_ja + ' ' + this.member.given_name_ja + ' | ' + this.member.role + ' | ' + this.meta.description },
         { hid: "og:title", property: "og:title", content: this.meta.title },
         { hid: "og:type", property: "og:type", content: this.meta.type },
         { hid: "og:url", property: "og:url", content: this.meta.url },
         { hid: "og:image", property: "og:image", content: this.meta.image },
         { name: "twitter:title", content: this.meta.title },
-        { name: "twitter:card", content: "summary" },
+        { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:site", content: "@tedxutsukuba" }
       ]
     }
