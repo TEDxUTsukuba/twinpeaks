fdbeab24ae20d822a111fcf0c965dab3229c4673
HarukiKinoshita <harukikinoshita@outlook.com>
2021-12-01 13:20:59 +0900

    Add conference pages (2017/2016)

--- pages/conferences/2017.vue
+++ pages/conferences/2017.vue
@@ -1,3 +1,24 @@
 <template>
-  
+  <section id="wrapper-dark" class="font-awesome">   
+    <section class="section has-text-centered-mobile">
+      <h1 class="subtitle is-4 is-family-narrow has-text-primary">TEDxUTsukuba 2017</h1>
+      <h1 class="title is-1">moving</h1>
+    </section>
+  </section>
 </template>
+
+<script>
+import Meta from '~/assets/mixins/meta'
+
+export default {
+  mixins: [Meta],
+  data(){
+    return {
+      locale: this.$i18n.locale,
+      meta: {
+        title: 'TEDxUTsukuba 2017 "moving"',
+      },
+    }
+  }
+}
+</script>
