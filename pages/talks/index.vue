5e27548c693904f9db925453cb3053b65c5cc0ba
TAMURAYuto <yuto.tamur@gmail.com>
2023-11-03 02:28:06 +0900

    [hotfix] no video speakers

--- pages/talks/index.vue
+++ pages/talks/index.vue
@@ -1,16 +1,19 @@
 <template>
   <section id="wrapper-dark">
     <section class="section has-text-centered is-halfheight">
-      <i class="mdi mdi-youtube has-text-primary is-1" /> 
-      <h1 class="title is-1 is-spaced">{{ $t('talks.title') }}</h1>
-      <h1 class="subtitle has-text-grey-light">{{ $t('talks.subtitle') }}</h1>
+      <i class="mdi mdi-youtube has-text-primary is-1" />
+      <h1 class="title is-1 is-spaced">{{ $t("talks.title") }}</h1>
+      <h1 class="subtitle has-text-grey-light">{{ $t("talks.subtitle") }}</h1>
     </section>
     <section class="section-wide">
       <div class="columns is-multiline">
         <div
           id="thumbnails"
           class="column is-one-third-fullhd is-half-widescreen is-half-desktop is-half-tablet"
-          v-for="(talk, index) in talks.filter(el => !el.eventName.name.match('Live'))" :key="index"
+          v-for="(talk, index) in talks.filter(
+            el => el.youtubeId && !el.eventName.name.match('Live')
+          )"
+          :key="index"
           style="display: inline-block; vertical-align: top;"
         >
           <div class="nmp-dark">
@@ -29,23 +32,62 @@
                     {{ talk.talkTitle }}
                   </h1>
                   <h1 class="subtitle is-5" v-if="$i18n.locale == 'en'">
-                    {{ talk.firstName }} {{ talk.middleName }} {{ talk.lastName }}
+                    {{ talk.firstName }} {{ talk.middleName }}
+                    {{ talk.lastName }}
                   </h1>
                   <h1 class="subtitle is-5" v-else>
-                    {{ talk.lastName }} {{ talk.middleName }} {{ talk.firstName }}
+                    {{ talk.lastName }} {{ talk.middleName }}
+                    {{ talk.firstName }}
                   </h1>
                 </div>
               </div>
-              <br>
+              <br />
               <div class="content">
-                <b-collapse animation="fade" :open="false" aria-id="contentIdForA11y1">
-                  <span class="" slot="trigger" slot-scope="props" aria-controls="contentIdForA11y1">
-                    <p class="is-size-7 has-text-grey-light" v-if="!props.open && $i18n.locale == 'ja'">{{ talk.description | substrBefore(75) }}…</p>
-                    <p class="is-size-7 has-text-grey-light" v-if="!props.open && $i18n.locale == 'en'">{{ talk.description | substrBefore(75) }}…</p>
-                    <p class="is-size-7 has-text-grey-light" v-if="props.open && $i18n.locale == 'ja'">{{ talk.description }}</p>
-                    <p class="is-size-7 has-text-grey-light" v-if="props.open && $i18n.locale == 'en'">{{ talk.description }}</p>
-                    <br>
-                    <b-button class="button is-gradient is-rounded is-outlined is-small" :icon-left="!props.open ? 'chevron-down' : 'chevron-up'">{{ !props.open ? $t('button.readmore') : $t('button.readless') }}</b-button>
+                <b-collapse
+                  animation="fade"
+                  :open="false"
+                  aria-id="contentIdForA11y1"
+                >
+                  <span
+                    class=""
+                    slot="trigger"
+                    slot-scope="props"
+                    aria-controls="contentIdForA11y1"
+                  >
+                    <p
+                      class="is-size-7 has-text-grey-light"
+                      v-if="!props.open && $i18n.locale == 'ja'"
+                    >
+                      {{ talk.description | substrBefore(75) }}…
+                    </p>
+                    <p
+                      class="is-size-7 has-text-grey-light"
+                      v-if="!props.open && $i18n.locale == 'en'"
+                    >
+                      {{ talk.description | substrBefore(75) }}…
+                    </p>
+                    <p
+                      class="is-size-7 has-text-grey-light"
+                      v-if="props.open && $i18n.locale == 'ja'"
+                    >
+                      {{ talk.description }}
+                    </p>
+                    <p
+                      class="is-size-7 has-text-grey-light"
+                      v-if="props.open && $i18n.locale == 'en'"
+                    >
+                      {{ talk.description }}
+                    </p>
+                    <br />
+                    <b-button
+                      class="button is-gradient is-rounded is-outlined is-small"
+                      :icon-left="!props.open ? 'chevron-down' : 'chevron-up'"
+                      >{{
+                        !props.open
+                          ? $t("button.readmore")
+                          : $t("button.readless")
+                      }}</b-button
+                    >
                   </span>
                 </b-collapse>
               </div>
@@ -58,9 +100,9 @@
 </template>
 
 <script>
-import Meta from '~/assets/mixins/meta'
-import Movie from '~/components/Movie';
-import { request, gql } from '~/lib/datocms'
+import Meta from "~/assets/mixins/meta";
+import Movie from "~/components/Movie";
+import { request, gql } from "~/lib/datocms";
 
 export default {
   mixins: [Meta],
@@ -84,29 +126,27 @@ export default {
           }
         }
       `
-    })
-    return { ready: !!data, ...data }
+    });
+    return { ready: !!data, ...data };
   },
   data() {
     return {
       locale: this.$i18n.locale,
       meta: {
-        title: this.$i18n.t('talks.title'),
-        description: this.$i18n.t('talks.subtitle')
-      },
-    }
+        title: this.$i18n.t("talks.title"),
+        description: this.$i18n.t("talks.subtitle")
+      }
+    };
   },
   filters: {
     substrBefore(text, value) {
-     return text.length > value ? text.slice(0, value) : text;
+      return text.length > value ? text.slice(0, value) : text;
     },
     substrAfter(text) {
-     return text.length > 50 ? text.substr(50) : text;
+      return text.length > 50 ? text.substr(50) : text;
     }
   }
-}
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
