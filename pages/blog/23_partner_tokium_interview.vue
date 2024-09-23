d5b2588a6920e16eb2b65829170d90d928dddaa2
makiart <haruki3968@gmail.com>
2024-09-23 11:30:06 +0900

    add 23 tokium interview blog

---
+++ pages/blog/23_partner_tokium_interview.vue
@@ -0,0 +1,173 @@
+<template>
+    <section id="wrapper-dark">
+        <section class="section has-text-centered is-halfheight">
+            <i class="mdi mdi-newspaper has-text-primary is-size-1" />
+            <h1 class="title is-2 is-spaced">株式会社TOKIUM 西本様 インタビュー</h1>
+            <p class="subtitle has-text-grey-light">
+                2023年TEDxUTsukubaイベント「万華鏡」にて協賛していただいた株式会社TOKIUMの西本様にお話を伺いました。
+            </p>
+            <img src="/blog/23_partner_tokium_interview/002.jpg" alt="株式会社TOKIUMの方とTEDxUTsukubaメンバーの集合写真" style="width: 100%; margin: 1rem 0;">
+        </section>
+        <section class="section">
+            <article class="article">
+                <p>今回は、株式会社TOKIUMの西本様へインタビューをさせていただきました。TEDxUTsukuba2024のイベントのお話や、株式会社TOKIUMに関するお話、そして今の大学生へ向けたコメントを頂戴いたしました。</p>
+
+                <h2>株式会社TOKIUMとは</h2>
+
+                <p><strong>山形</strong>：まずは、ブログを見てくださっている方に向けて、株式会社TOKIUMについて軽く、ご説明を頂きたいです！</p>
+
+                <p><strong>西本様</strong>：我々は業務効率化、DX*を実現するサービスを提供しています。無駄な時間を減らして、未来へつながる「時間を生み出すインフラ」を目指しています。社名であるTOKIUMも「未来へつながる時を生む」から来ています。</p>
+
+                <p>目指していることは社会のインフラとなることで、社会の課題をテクノロジーや自由な発想と時には泥臭さも厭わず解決していこうと、その志を皆が体現しています。僕が以前関わっていたゲーム開発の世界だと、時の運やコンテンツ力が絡むのに対して、テクノロジーによる力で課題をより直接的に解決できると感じています。</p>
+
+                <p>※ DXとは企業が、ビッグデータなどのデータとAIやIoTを始めとするデジタル技術を活用して、業務プロセスを改善して組織、企業文化、風土をも改革し、競争上の優位性を確立すること。</p>
+
+                <h2>ゲーム開発とキャリアについて</h2>
+
+                <p><strong>山形</strong>：次に経歴についてお聞かせください。</p>
+
+                <p><strong>西本様</strong>：2010年に、筑波大学情報メディア創成学類に入学しました。元々はシンプルにゲームを作りたかったんですよ。ゲームを作る＝プログラミングだ、みたいな。その頃は全然情報系とか詳しくなくて、リテラシーも全然なくて。そういう状況だったので何となくプログラミングができるところに入りたいなという感覚で大学に入って、プログラミングを勉強し始めました。大学外でも、ゲームを簡単なものから作ったりとか、ゲーム開発のバイトをしたりしていました。また、仲間内で筑波大の学祭向けにゲームを作って出すという活動をしていました。</p>
+
+                <p>やっぱりゲーム開発は楽しいなという考えがあったし、大学院に行く選択肢は僕の中でほぼなかったので、ゲーム業界へ就職をしようと思っていました。もっと大きなゲーム開発かつ、多くの人に触ってもらえるような開発をしたいと思って、前職のゲーム会社に入りました。結局3年くらい会社に勤めたのですが、ゲーム開発は厳しいんだなと感じるようになっていきました。</p>
+
+                <p>いくつかプロジェクトに関わったのですが、世に出ずプロジェクトが終了することもしばしば。出しても長続きせずサービス終了というのが続き、たくさんの人と関わったりする中で、「誰とやるか」が重要になっていき、今の会社に入りました。このTOKIUMという会社は筑波大学の同期が立ち上げた会社で、友人関係でもあったし、ずっと声をかけてもらっていたので、シンプルに楽しそうだと思い入社しました。</p>
+
+                <p><strong>山形</strong>：高校のときに文系で、ゲーム作りのために筑波大学を選んだことで、在学中大変だったそうですね。</p>
+
+                <p><strong>西本様</strong>：元々は大阪大学の外国語学部を志望していて、センター入試の結果で変えたんですよね。そのまま受けても良かったし、選んだあとに何をするかの方が重要だと思っていたので、強い希望があって筑波大に入ったわけではないです。センターが終わったあとに数3Cの勉強をして、何とかなりました。(笑)</p>
+
+                <p><strong>山形</strong>：在学中には、ゲーム開発をするために1年間休学したとうかがいました</p>
+
+                <p><strong>西本様</strong>：3年から4年に上がるタイミングで1年間休学をしました。ゲーム開発のバイトをその1年間にがっつりやって、何か自分でゲームを作ったというよりかは、会社に入ってもうめちゃくちゃやりました。</p>
+
+                <img src="/blog/23_partner_tokium_interview/001.jpg" alt="木下様の話を伺う富田と田中" style="width: 100%; margin: 1rem 0;">
+
+                <h2>学生に向けて</h2>
+
+                <p><strong>髙橋</strong>：今の大学生に向けて、TOKIUMさんとして何かコメントを頂きたいです。</p>
+
+                <p><strong>西本様</strong>：今って、めちゃくちゃ選択肢があって皆さんどうやって進路とか決めているんですかね？</p>
+
+                <p><strong>髙橋</strong>：めっちゃ悩んでいます。(笑)</p>
+
+                <p><strong>西本様</strong>：そうですね。学生起業も増えてきたし、YouTuberをやるって人もいるだろうし。エンタメとかそういう業界や業種、ポジションも増えているから、物事を作るのはエンジニア以外もいっぱいあるし...。皆さんどのようなイメージで就活しているんでしょうか？</p>
+
+                <p><strong>髙橋</strong>：そうですね...。個人的な感覚ですが、大学生は3年生から就活をしていると思っています。自分たちの興味を分析してインターンに行くなりしていると思います。想像が足りてない感覚は自分にもありました。</p>
+
+                <p><strong>西本様</strong>：やっぱりやってみないと分からないことが多いと思うんですよ。入学して1年や2年経たないうちに皆さんの考え方は変わると思うんです。ポジティブな方向に変わるかもしれないし、ネガティブに変わるかもしれないですが、とにかく変わるんですよ。</p>
+
+                <p>自分の心や取り巻く環境や見え方、情勢も変わります。変わるという前提に立って、やりたいことを決めて欲しいなと思います。あまり難しく考えずに、自分のファーストインプレッションでもいいんじゃないかなと。その意思決定も大事だけど、入った後で自分がどれだけ頑張れるか、自分が頑張れそうか、も大事だと思う。どれだけがむしゃらでやるか。入ったときにどうするかを考えて動くというのもいいんじゃないかなと思いました。</p>
+
+                <p><strong>髙橋</strong>：そういう意味でいうとTOKIUMさんの志にある「泥臭くやる」というワードと通じているなと感じました。TOKIUMさんとしてはどういう学生を求めているんですか？</p>
+
+                <p><strong>西本様</strong>：志に共感してもらえる人はもちろん、チームのみんなと一緒にお客様、共に働く仲間それぞれにホスピタリティを持ちながら仕事に取り組み、果敢に挑戦できる方を歓迎しています。これからどんどんTOKIUMは伸びていくと思いますし、どんどん変化を遂げていくと思います。そんな会社だからこそ挑戦できる環境を提供できます。</p>
+
+                <img src="/blog/23_partner_tokium_interview/003.jpg" alt="木下様の話を伺う富田と田中" style="width: 100%; margin: 1rem 0;">
+
+                <h2>TEDxUTsukubaについて</h2>  
+                <p><strong>髙橋</strong>：どうしてTEDxUTsukubaに関わってくださったのですか？</p>
+
+                <p><strong>西本様</strong>：正直とりあえずやるかみたいな(笑)。つくばにいい人がいっぱいいるなってのは肌で感じていて、丁度つくばに還元できるものがないか考えていました。そのいいきっかけになったと思っていますし、参加して良いイベントだなと思いました。</p>
+
+                <p><strong>髙橋</strong>：ありがとうございます。よりよいコミュニティになっていけるよう精進します！<br>  
+                本日は貴重なお時間をいただきありがとうございました！</p>
+
+                <hr>
+                <h2>プロフィール</h2>
+                <p>株式会社TOKIUM（<a href="https://corp.tokium.jp/">ウェブサイト</a>）：プロダクト本部開発部　部長　西本貴様（<a href="https://twitter.com/nsmttwi">@nsmttwi</a>）</p>
+
+                <p>インタビュアー：TEDxUTsukuba　山形・髙橋<br>  
+                記事編集・作成：TEDxUTsukuba　髙橋</p>
+            </article>
+        </section>
+    </section>
+</template>
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: "スカイランドベンチャーズ株式会社 木下様 インタビュー",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "2023年TEDxUTsukubaイベント「万華鏡」にて協賛していただいたスカイランドベンチャーズ株式会社の代表を務める木下様にお話を伺いました。",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "スカイランドベンチャーズ株式会社 木下様 インタビュー",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "2023年TEDxUTsukubaイベント「万華鏡」にて協賛していただいたスカイランドベンチャーズ株式会社の代表を務める木下様にお話を伺いました。",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "https://tedxutsukuba.com/blog/23_partner_skylandventures_interview/002.jpg",
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: "スカイランドベンチャーズ株式会社 木下様 インタビュー",
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content:
+            "2023年TEDxUTsukubaイベント「万華鏡」にて協賛していただいたスカイランドベンチャーズ株式会社の代表を務める木下様にお話を伺いました。",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: "https://tedxutsukuba.com/blog/23_partner_skylandventures_interview/002.jpg",
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+hr {
+  height: 1px;
+}
+a {
+  color: #fff;
+  font-weight: bold;
+}
+
+.article h2 {
+  margin-top: 1.5rem;
+  font-size: 2rem; /* Increase the font size for more prominence */
+  font-weight: bold;
+  margin-bottom: 1rem;
+  color: #fff; /* Set the color to white for visibility in dark mode */
+}
+
+a {
+  color: hsl(217, 71%, 53%);
+}
+
+a:hover {
+  color: hsl(217, 71%, 63%);
+  text-decoration: underline;
+}
+
+strong {
+  font-weight: bold;
+  color: #FFF;
+}
+
+p {
+  color: #ddd !important;
+  line-height: 2rem;
+  margin-bottom: 1rem;
+}
+</style>
