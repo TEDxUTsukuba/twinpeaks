18c238af343297058a9aec7bcd96bb607263f4dc
horiyu <horiyu.dev@gmail.com>
2025-03-11 11:24:11 +0900

    add: 24 tokium interview blog

---
+++ pages/blog/24_partner_tokium_interview.vue
@@ -0,0 +1,158 @@
+<template>
+    <section id="wrapper-dark">
+        <section class="section has-text-centered is-halfheight">
+            <i class="mdi mdi-newspaper has-text-primary is-size-1" />
+            <h1 class="title is-2 is-spaced">株式会社TOKIUM 西本様 インタビュー</h1>
+            <p class="subtitle has-text-grey-light">
+                2024年TEDxUTsukubaイベント「Panorama」にて協賛していただいた株式会社TOKIUMでインターンをされている大澤様にお話を伺いました。
+            </p>
+            <img src="/blog/24_partner_tokium_interview/001.jpg" alt="株式会社TOKIUMでインターン中の大澤さんとTEDxUTsukubaメンバーの集合写真" style="width: 100%; margin: 1rem 0;">
+        </section>
+        <section class="section">
+            <article class="article">
+                <p>今回は、株式会社TOKIUMでインターンをされている筑波大生の大澤さんへインタビューをさせていただきました。インターンの内容に関するお話や、大澤さんご自身のお話、そして筑波大生に向けたメッセージを頂戴しました。</p>
+                <p>株式会社TOKIUMは、支出管理クラウド「TOKIUM」によって業務効率化、DX*を実現するサービスを提供しています。</p>
+                <p>※ DXとは企業が、ビッグデータなどのデータとAIやIoTを始めとするデジタル技術を活用して、業務プロセスを改善して組織、企業文化、風土をも改革し、競争上の優位性を確立すること。</p>
+
+                <h2>自己紹介</h2>
+
+                <p><strong>西田</strong>: まずは自己紹介をお願いします。</p>
+                <p><strong>大澤さん</strong>: 大澤怜二（おおざわれいじ）と申します。筑波大学の情報学群情報メディア創成学類の4年生です。大学院に進学予定で、4月からは、情報理工学位プログラムに進学する予定です。</p>
+                <p><strong>西田</strong>: もともとは総合学域群の1期生だったそうですね。</p>
+                <p><strong>大澤さん</strong>: そうですね。入学前は知識情報・図書館学類に興味を持っていて、前期試験で受験することを考えたときに、総合学域群への進学を決めました。読書好きが転じて図書館司書に興味を持ったときに、筑波大学を知ったのがきっかけですね。入学して色々な授業を受けて、多くの学類で迷った末に、情報メディア創成学類への移行を決めました。</p>
+                <p><strong>西田</strong>: そうだったんですね。情報メディア創成学類に移行してからはどのような勉強・研究をされているんですか。</p>
+                <p><strong>大澤さん</strong>: メディアやプログラミング、デザインなどをはじめとした幅広い分野の勉強をしていました。私は現在、インタラクティブプログラミング研究室に所属しています。専門は、HCI（ヒューマンコンピュータインタラクション）といって、人間とコンピュータの相互作用を扱っています。特に、コンピュータを用いることによって人間同士のコミュニケーションを円滑にすることをテーマに研究をしています。</p>
+
+                <img src="/blog/24_partner_tokium_interview/002.jpg" alt="西田と今井と話す大澤さん" style="width: 100%; margin: 1rem 0;">
+
+                <h2>株式会社TOKIUMでインターンを始めたきっかけ</h2>
+
+                <p><strong>西田</strong>: 株式会社TOKIUMでインターンを始められたのはいつですか。</p>
+                <p><strong>大澤さん</strong>: 3年生の夏からです。Startup Weekendというイベントに参加した時に、そこのスポンサーピッチをされていたTOKIUMのCTOである西平さんとお話をして、声をかけていただいたのがきっかけです。</p>
+                <p><strong>西田</strong>: ちなみに普段はどのような業務をされていますか。</p>
+                <p><strong>大澤さん</strong>: 最近は新卒採用がメインです。選考や面接というよりかは、メールでのやり取りとか、Paizaやサポーターズといった媒体に求人を出したり、イベントをしたりするためのやり取りをしています。また、最近つくばにできた株式会社TOKIUMつくば拠点の管理人みたいなこともやってます笑</p>
+                <p><strong>西田</strong>: そうなんですね。インターンの業務を通じて、ご自身が成長を実感されたポイントなどはありますか。</p>
+                <p><strong>大澤さん</strong>: わからないところは質問しながらも、結構業務を任せてもらっているので、責任ややりがいはとても感じています。一番自信がついたと思う出来事としては、去年の夏に初めてサマーインターン2024を開催したことですね。3、4月くらいにCTOの西平さんに任せていただいて、当時はTOKIUMのつくば拠点も含めて、何もない状態からのスタートでした。他の社員さんとも相談しながら、物件探しから求人、面接選考まで一通りやらせていただいたのが、アウトプットの機会として良い経験になったかなと思います。</p>
+                <p><strong>西田</strong>: インターンをする上で何か大事にされていることはありますか。</p>
+                <p><strong>大澤さん</strong>: 適宜、共有・相談をするのは絶対大事だと思います。これどうなってましたっけ。といった質問をリストアップして整理して、粗がないかを何回も見直すようにしています。</p>
+
+                <h2>これからの展望</h2>
+
+                <p><strong>西田</strong>: 4月から大学院に進学されるということですが、将来についての展望はございますか。</p>
+                <p><strong>大澤さん</strong>: 職種についてはまだ迷っています。ただ一つ言えるのは、一緒に働く人に対して、良いなと思える場で働きたいということですね。さっきも言った通り、適宜確認をすることが大事になってくるので、そのようなコミュニケーションをとることができる関係性を構築できていないと、仕事も円滑にまわらないし、なによりやっていて楽しくないと思います。コミュニケーションを取りやすい雰囲気や人柄が揃った環境が良いなというふうに思っています。</p>
+                <p><strong>西田</strong>: なるほど。やはりどの職種でもコミュニケーションは大切ですよね。</p>
+                <p><strong>大澤さん</strong>: 仕事の話だけではなくて、雑談ができる関係性っていうのは次につながるんじゃないかと思いますね。どうやったらそういう関係性ができるのかは難しいところですね笑</p>
+
+                <h2>TEDxUTsukuba 2024年度イベントに参加された感想</h2>
+                <p><strong>西田</strong>: TEDxUTsukuba 2024年度イベント「Panorama」に参加されてみて、何か感じられたことはございますか。</p>
+                <p><strong>大澤さん</strong>: イベントは面白かったです。特に、WSのグループをランダムで組むことができたのはよかったと思っています。自分からコミュニケーションを取りにくい人にとって、知らない人と話せる機会を作ってもらえたのはありがたかったですね。さらに不特定多数の人と話すことができるようになればもっと良いイベントになると思います。</p>
+
+                <img src="/blog/24_partner_tokium_interview/003.jpg" alt="大澤さんの話を伺う西田と今井" style="width: 100%; margin: 1rem 0;">
+
+                <h2>筑波大生に伝えたいこと</h2>
+                <p><strong>西田</strong>: 私も含めて、インターンの応募になかなか踏み出せずにいる大学生は多くいると思いますが、そのような方々に向けて大澤さんから何かメッセージをいただきたいです。</p>
+                <p><strong>大澤さん</strong>: 興味があることに足を踏み出してみることが大事だと、ここ数年特に感じています。私自身、「最近はノリで生きています」ということが多くて、迷ったらやってみなよ。っていうのをとても伝えたいですね。面白そうだな、楽しそうだなと思ったところには足を運んでみるとか。体験してみてから、合うかどうかを判断すれば良いと思いますし、その方が選択の幅も広がると思っています。</p>
+                <p><strong>西田</strong>: ありがとうございます。株式会社TOKIUMのインターン生として、大澤さんから筑波大生に何か伝えたいことはございますか。</p>
+                <p><strong>大澤さん</strong>: つくば拠点をもっと活用してほしいです。インターンだけではなく、開発を最初にチャレンジできる場として社員やインターン生に質問できる環境も揃っているので、もっと気軽に来てほしいですね。自分が興味がある分野に一歩踏み出す場として、つくば拠点がきっかけになると思うので、ぜひ来てください。</p>
+
+                <h2>プロフィール</h2>
+                <p>筑波大学 情報学群 情報メディア創成学類 4年 大澤 怜二 様</p>
+                <p>インタビュアー: TEDxUTsukuba 西田</p>
+                <p>記事編集・作成: TEDxUTsukuba 西田・今井</p>
+                <p>株式会社TOKIUMつくば拠点（ウェブサイト）:</p>
+                <p>茨城県つくば市春日3丁目1-12 KMビル 2階</p>
+                <p><a href="https://maps.app.goo.gl/SZ5p48YH7FY1DGXi9">https://maps.app.goo.gl/SZ5p48YH7FY1DGXi9</a></p>
+            </article>
+        </section>
+    </section>
+</template>
+
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: "株式会社TOKIUM 大澤様 インタビュー",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "2024年TEDxUTsukubaイベント「Panorama」にて協賛していただいた株式会社TOKIUMでインターンをされている大澤様にお話を伺いました。",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "株式会社TOKIUM 大澤様 インタビュー",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "2024年TEDxUTsukubaイベント「Panorama」にて協賛していただいた株式会社TOKIUMでインターンをされている大澤様にお話を伺いました。",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "https://tedxutsukuba.com/blog/23_partner_skylandventures_interview/002.jpg",
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: "株式会社TOKIUM 大澤様 インタビュー",
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content:
+            "2024年TEDxUTsukubaイベント「Panorama」にて協賛していただいた株式会社TOKIUMでインターンをされている大澤様にお話を伺いました。",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: "https://tedxutsukuba.com/blog/23_partner_skylandventures_interview/002.jpg",
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+hr {
+  height: 1px;
+}
+a {
+  color: #fff;
+  font-weight: bold;
+}
+
+.article h2 {
+  margin-top: 1.5rem;
+  font-size: 2rem; /* Increase the font size for more prominence */
+  font-weight: bold;
+  margin-bottom: 1rem;
+  color: #fff; /* Set the color to white for visibility in dark mode */
+}
+
+a {
+  color: hsl(217, 71%, 53%);
+}
+
+a:hover {
+  color: hsl(217, 71%, 63%);
+  text-decoration: underline;
+}
+
+strong {
+  font-weight: bold;
+  color: #FFF;
+}
+
+p {
+  color: #ddd !important;
+  line-height: 2rem;
+  margin-bottom: 1rem;
+}
+</style>
